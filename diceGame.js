ff2730515a234e13208bbdcb7dd37b74f2d6ec3a
unknown <kamjohn79.kdj@gmail.com>
2023-07-02 19:47:42 -0500

    added some more test cases, updated and solved one of the test cases in our diceGame.js file, also added some json packages

---
+++ diceGame.js
@@ -0,0 +1,10 @@
+
+function isWinningRoll() {
+    diceNums = [2,3,4,5,6,7,8,9,10,11,12];
+    const randomRoll = Math.floor(Math.random() * diceNums.length)
+
+}
+
+const playDiceGame = (player1, player2) => {
+
+}
